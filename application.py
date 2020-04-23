3d1b26bd73d28292b69239348f1aba67f8b65c61
Zuiru <2425277916@qq.com>
2020-04-23 17:12:18 +0800

    自定义url处理类&&数据库创建

--- application.py
+++ application.py
@@ -2,21 +2,21 @@ from flask import Flask
 from flask_script import Manager
 import os
 
-# 修改flask 类
+# 修改Flask类
 class Application(Flask):
     def __init__(self,import_name,template_folder=None,root_path=None,static_folder=None):
         super(Application,self).__init__(import_name=import_name,template_folder=template_folder,root_path=root_path,static_folder=static_folder)
 
 
 
-app = Application(__name__,template_folder=os.getcwd()+'/web/templates/',root_path=os.getcwd(),static_folder=os.getcwd()+"/web/static/")
-
+app = Application(__name__,template_folder=os.getcwd()+"/web/templates/",root_path=os.getcwd(),static_folder=os.getcwd()+"/web/static/")
 manager = Manager(app)
 
-
-
-
-
+# 函数模板
+from common.libs.UrlManager import UrlManager
+app.add_template_global(UrlManager.buildUrl,'buildUrl')
+app.add_template_global(UrlManager.buildStaticUrl,'buildStaticUrl')
+app.add_template_global(UrlManager.buildImageUrl,'buildImageUrl')
 
 
 
